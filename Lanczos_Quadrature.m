b9338132d19f94b7b9ee097fc5282ce0b91b1375
Justin9872 <168428306+Justin9872@users.noreply.github.com>
2026-01-29 11:14:53 +0700

    Update Lanczos_Quadrature.m

--- Lanczos_Quadrature.m
+++ Lanczos_Quadrature.m
@@ -1,31 +1,32 @@
-function [Im,theta,tau,T] = Lanczos_Quadrature(A, u, m, n)
-%LANCZOS_QUADRATURE(A, u, m, n) computes a symmetric tridiagonal matrix by 
-% Lanczos iteration.
+function [Im,theta,tau,T] = Lanczos_Quadrature(A, u, m)
+%LANCZOS_QUADRATURE(A, u, m) computes a symmetric tridiagonal matrix by 
+% Lanczos iteration. Besides, in this paper, this method helps estimate the
+% quadratic form with respect to exponential matrix, expm(A).
 %
 %  Input:
 %  A: positive semi-definite matrix
 %  u: initial vector
 %  m: Lanczos iteration steps
-%  n: dimension of u and A
 %
 %  Ouput:
-%  Im: the value of quadratic form u'log(A)u
+%  Im: the value of quadratic form u'expm(A)u
 %  theta: Gaussian quadrature nodes
 %  tau: Gaussian quadrature weights
 %  T: symmetric tridiagonal matrix
 %
 %  Usage (example):
 %  A = gallery('wathen',10,10); u = ones(length(A),1); 
-%  T = Lanczos(A,u,100);
+%  [Im, ~, ~, T] = Lanczos_Quadrature(A,u,100);
 %
-%  Copyright: Shengxin Zhu, Beijing Normal University; 
-%             Wenhao Li, HongKong Baptist University.
+%  Copyright: Shengxin Zhu, Beijing Normal University
 
+n = length(A);
 alpha = zeros(m,1);
 beta = zeros(m+1,1);
 xold = zeros(n,1);
 normu = norm(u);
 x = u./normu;
+
 for i = 1:m
     y = A*x;
     alpha(i) = dot(x,y);
@@ -37,6 +38,7 @@ for i = 1:m
     xold = x;
     x = eta/beta(i+1);
 end
+
 % Obtain symmetric tridiagonal matrix T.
 T = diag(alpha) + diag(beta(2:end-1),1) + diag(beta(2:end-1),-1);
 
